07c48c99e89e5fa0782bf246e1e6877ff92b22aa
Antonielnsp <anrfotografia@gmail.com>
2021-06-16 16:01:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xlsx-para-gmax-psqnat
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/xlsx-para-gmax-psqnat)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'XLSX para GMAX';
+  willDownload = false;
+
+  constructor() { }
+
+  onFileChange(ev) {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      const dataString = JSON.stringify(jsonData);
+      document.getElementById('output').innerHTML = dataString.slice(0, 0).concat("...");
+      this.setDownload(dataString);
+    }
+    reader.readAsBinaryString(file);
+  }
+
+
+  setDownload(data) {
+    this.willDownload = true;
+    setTimeout(() => {
+      const el = document.querySelector("#download");
+      el.setAttribute("href", `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+      el.setAttribute("download", 'Lista_de_Usuários.gmax');
+    }, 1000)
+  }
+
+
+
+
+}
